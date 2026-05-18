59fee7339ed8f42e4a678d5e935ba6d82420ca80
samarth-chawla <samarthchawla91@gmail.com>
2026-05-18 18:44:16 +0530

    add Vercel Analytics and Speed Insights to enhance performance tracking

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,14 @@ import Experience from "@/sections/Experience";
 import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
 import Projects from "@/sections/Projects";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
     <main className="overflow-x-clip">
+      <Analytics />
+      <SpeedInsights />
       <Hero />
       <About />
       <Projects />
